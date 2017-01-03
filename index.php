895ae2a1cbf7fd8e7f88669468f42bf1deb9b7f3
cloudasim <cloudasims@gmail.com>
2017-01-03 16:16:30 +0545

    First Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>APP - Learning jquery AJAX</title>
+        <meta name="description" content="ajax jquery">
+        <meta name="author" content="cloudasim">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="assets_front/images/favicon.ico">
+        <link rel="stylesheet" href="assets/dist/css/main.min.css" type="text/css">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400|Merriweather:300,400,700">
+    </head>
+    <!--[if lt IE 7]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <body>
+        <div id="main-wrapper">
+            
+        </div>
+    </body>
+    <script src="assets/dist/js/script.min.js" type="text/javascript"></script>
+</html>
